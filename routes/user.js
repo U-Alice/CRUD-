654e27223ec7a5888b907a832c333b8ab6d8c73f
U-Alice <umugwanezaalice22@gmail.com>
2022-02-02 14:16:42 +0200

    updated paths

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-import {createUser, getUsers, getUser, deleteUser, updateUser} from '../../controllers/users.js'
+import {createUser, getUsers, getUser, deleteUser, updateUser} from '../controllers/users.js'
 
 
 const router = express.Router();
